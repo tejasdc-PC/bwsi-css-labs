5c48081e99956ace24b62d9d3e14cc6f611a510b
tejasdc-PC <tejasdurgamchen@gmail.com>
2026-03-25 10:30:21 -0400

    Variable Speed Change
    Altered to 3ms

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,12 +4,12 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
-
+    # variable speed name:3ms
 def main():
     print("Hello World!")
     print("Hi, my name is:")
     name = "Tejas Durgam-Chen" # TODO: Insert your name between the double quotes
-    # variable speed name
+
 
     print(f"{name}, Welcome to the CSS course!")
 
